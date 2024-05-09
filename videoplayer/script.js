87f10c6807e06cfd431e1a118ddf38afa2382c08
diemmi <131148069+diiemmi@users.noreply.github.com>
2024-05-09 19:52:45 +1000

    f

---
+++ videoplayer/script.js
@@ -0,0 +1,22 @@
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+
+const playPauseButton = document.querySelector("#play-pause-button");
+console.log(playPauseButton);
+
+playPauseButton.addEventListener("click", playPauseVideo);
+
+const playPauseImg = document.querySelector("click", playPauseVideo)
+
+const playPauseImg = document.querySelector("#play-pause-img");
+
+
+function playPauseVideo() {
+if (myVideo.paused || myVideo.ended) { 
+   playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+    myVideo.play();
+} else {
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+    myVideo.pause();
+}
+}
