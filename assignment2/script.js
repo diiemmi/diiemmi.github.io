b62538fe269233e1164d093bf011cbed42f95d68
diemmi <131148069+diiemmi@users.noreply.github.com>
2024-05-20 20:59:30 +1000

    g

---
+++ assignment2/script.js
@@ -0,0 +1,29 @@
+// const video = document.querySelector("#custom-video-player");
+// const playPauseBtn = document.querySelector("#play-pause-btn");
+// const playPauseImg = document.querySelector("#play-pause-img");
+// const progressBar = document.querySelector("#progress-bar-fill");
+// video.removeAttribute("controls");
+// // playPauseBtn.addEventListener("click", togglePlayPause);
+// video.addEventListener("timeupdate", updateProgressBar);
+// function togglePlayPause() {
+//   if (video.paused || video.ended) {
+//     video.play();
+//     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+//   } else {
+//     video.pause();
+//     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+//   }
+// }
+// function updateProgressBar() {
+//   const value = (video.currentTime / video.duration) * 100;
+//   progressBar.style.width = value + "%";
+// }
+// Add other functionalities here
+
+// We create an object array containing the videos
+const videoList = [
+    { name: "Zenscape", link: "rain.mp4" },
+    { name: "Stardust", link: "water.mp4" },
+  ];
+  
+  let loop = false;
